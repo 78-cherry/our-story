1970b4b091bdcf31a3e4efe90c31574e5a9c0f40
78-cherry <poojatkanade@gmail.com>
2026-06-06 12:59:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.getElementById("enterBtn").addEventListener("click", function() {
 
+    document.getElementById("bgMusic").play();
+
+    document.getElementById("welcome-screen").style.display = "none";
+
+    document.getElementById("main-content").style.display = "block";
+
+});
